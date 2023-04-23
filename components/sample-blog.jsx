3c3eb133e6b8928530b7aca33ebc29ace11455a8
phongdatgl <phongdatgl@gmail.com>
2023-04-23 10:09:09 +0700

    meta tag

--- components/sample-blog.jsx
+++ components/sample-blog.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react'
+import Head from 'next/head';
 import axios from 'axios';
 export default function SamplePage (url) {
   const { asPath, pathname } = useRouter();
@@ -11,11 +12,29 @@ export default function SamplePage (url) {
   const getContent = async () => {
     const url = "https://laptrinhvn.net/api.php?post_name=" + asPath.replace('/', '')
     const response = await axios.get(url)
-    setContent(response.data.post_content)
+    setContent(response.data)
     
   }
   const rawMarkup = (raw) => {
         return { __html: raw }
     }
-  return (<div dangerouslySetInnerHTML={rawMarkup(content)} />)
+  return (
+    <div>
+      <Head>
+        <title>{content.post_title}</title>
+        <meta name="description" content={content.sub_content} />
+        <meta property="og:title" content={content.post_title} />
+        <meta
+          property="og:description"
+          content={content.sub_content}
+        />
+        <meta
+          property="og:image"
+          content={content.image}
+        />
+      </Head>
+      <div dangerouslySetInnerHTML={rawMarkup(content.post_content)} />
+    </div>
+
+    )
 }
