7a7594cd557f851f9f6b1a7c9f90e7d1be80a03b
phongdatgl <phongdatgl@gmail.com>
2023-04-23 08:42:04 +0700

    done api

--- components/sample-blog.jsx
+++ components/sample-blog.jsx
@@ -3,28 +3,19 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 export default function SamplePage (url) {
   const { asPath, pathname } = useRouter();
-  const [content, setContent] = useState({})
+  const [content, setContent] = useState('')
   useEffect(() => {
         getContent()
     }, [])
-  const getData = async (url) => {
-    const response = axios.get(url, {'Access-Control-Allow-Origin': '*', "Content-Type": "application/json", 'Accept': "application/json"})
-    return response
-    
- }
 
   const getContent = async () => {
     const url = "https://laptrinhvn.net/api.php?post_name=" + asPath.replace('/', '')
-    const response = await getData(url)
-    console.log(response)
-    setContent(response)
+    const response = await axios.get(url)
+    setContent(response.data.post_content)
     
   }
   const rawMarkup = (raw) => {
         return { __html: raw }
     }
-  // return (
-  //   <p>Get content from domain.com and path {content} </p>
-  //   )
   return (<div dangerouslySetInnerHTML={rawMarkup(content)} />)
 }
